4196d664d163079e0d9f949219047520f3ccba00
Matvey Solontsov <soloncovmatvey@gmail.com>
2024-11-09 10:57:13 +0300

    091124

---
+++ set2/set2.cpp
@@ -0,0 +1,19 @@
+/*
+set
+teacher - Aleksandr Savushkin
+091124
+*/
+#include <iostream>
+using namespace std;
+
+int a = 5;
+int b = 2;
+
+
+int main()
+{
+    a = a + 1;
+    int result = a - b;
+    cout << result << endl;
+    return 0;
+}
