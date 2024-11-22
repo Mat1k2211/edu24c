8f0d4b78253b0aa38ec17113ea63e3a9ad0bf4f3
Matvey Solontsov <soloncovmatvey@gmail.com>
2024-11-22 17:23:54 +0300

    ОГЭ 22.11.24

---
+++ oge/oge37831.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int size = 0;
+int number = 0;
+int maxx = 0;
+cout << "Введите количество чисел:";
+cin >> size;
+int array[size] = {};
+int c = 0;
+for (int i = 1; i <= size; i++)
+{
+    cout << "Введи " << i << " число: ";
+    cin >> array[i];
+}
+
+for (int j = 0; j < size; j++)
+{
+    if (array[j] % 6 == 0)
+    {
+        if (array[j] > maxx)
+        {
+            maxx = array[j];
+        }
+        
+    }
+    
+}
+
+cout << maxx << endl;
+return 0;
+
+}
