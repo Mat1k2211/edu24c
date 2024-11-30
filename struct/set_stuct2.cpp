55334f79e99a54d301df98428335abd1f9ee8d64
Matvey Solontsov <soloncovmatvey@gmail.com>
2024-11-30 12:11:55 +0300

    Struct 30.11.2024

--- struct/set_stuct2.cpp
+++ struct/set_stuct2.cpp
@@ -14,10 +14,12 @@ struct Employee
 int main()
 {
     Employee Matvey {"Matvey", "oscar-pc10", "The Palace of Children's creativity 4", 79870130394, "soloncovmatvey@gmail.com"};
-    cout << Matvey.name << endl;
-    cout << Matvey.id_pc << endl;
-    cout << Matvey.office << endl;
-    cout << Matvey.tel << endl;
-    cout << Matvey.Email << endl;
+
+    cout << "Name: " << Matvey.name << endl;
+    cout << "Id pc: " << Matvey.id_pc << endl;
+    cout << "Office: " << Matvey.office << endl;
+    cout << "Tel: " << Matvey.tel << endl;
+    cout << "Email: " << Matvey.Email << endl;
+    
     return 0;
 }
